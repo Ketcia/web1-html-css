ab0862740cb4fb404758332272bb8f44e108ea7a
Ketcia <leticiarafael98@gmail.com>
2021-08-29 09:22:10 -0300

    Commit 28/08

---
+++ projeto04/src/styles/GlobalStyle.ts
@@ -0,0 +1,31 @@
+import {createGlobalStyle} from "styled-components"
+
+
+export default createGlobalStyle`
+    @font-face {
+        font-family: 'Gemunu Libre';
+        font-style: normal;
+        font-weight: 400;
+        src:url('assets/fonts/GemunuLibre-Regular.woff') format('woff');
+    }
+*{
+    background-color: #363636;
+    margin: 0;
+    border: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+    font-family: "Gemunu Libre"
+}
+`
+
+export const colors = {
+    darkBg: "#363636",
+    black: "#000000",
+    grey: "#D9D9D9",
+    dark: "#262626",
+    dkGrey: "#595959",
+    dkBlue: "#243040",
+    white: "ffffff"
+
+}
